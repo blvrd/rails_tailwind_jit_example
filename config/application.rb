7836773e9f59ec93b0c473e5e4b29a26f9a0917e
Garrett Martin <garrett@blvrd.co>
2021-03-22 19:06:32 -0500

    Tailwind JIT setup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module RailsTailwindJitExample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    server do
+      Process.spawn "npm run css:watch"
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./**/*.{erb,js}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
